71caf22edbae9d4b7f6f1099fbab35a0f58425a4
hurtuh <46876669+hurtuh@users.noreply.github.com>
2021-02-14 11:49:08 +0300

    first commit

---
+++ Models/ChatEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServer.Models
+{
+    public class ChatMessage
+    {
+        public string text { get; set; }
+
+        public int id { get; set; }
+    }
+
+
+
+    public class ChatEvent
+    {
+        public string EventType { get; set; }
+
+        public ChatMessage Body { get; set; }
+    }
+}
